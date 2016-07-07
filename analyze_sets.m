c57d4d443f0105c439177499791a02b352561cbd
jwheelsc <jcrompto@sfu.ca>
2016-07-07 13:33:41 -0700

    here was a test branch that was created to debug the densify_lines function

--- analyze_sets.m
+++ analyze_sets.m
@@ -4,8 +4,8 @@ close all
 folder = 'D:\Field_data\2013\Summer\Images\JWC\GL1\Photogrammetry\July17\GL1PG1ST1\IMG_9030_analysis\'
 load([folder 'sets.mat'])
 
-setNum = 's1'
-SN = s1;
+setNum = 's2'
+SN = s2;
 
 %% get length of line in pixels
 
--- densify_lines.m
+++ densify_lines.m
@@ -1,41 +1,16 @@
-function [output] = densify_lines(set)
+%%% this function is called on from analyze_sets to densify the lines that
+%%% were created using imfreehand
 
-    xx = linspace(min(set(:,1)),max(set(:,1)),length(set(:,1))*25);
+function [output] = densify_lines(jset)
 
-    y = set(:,2);
-    x = set(:,1);
+    xx = linspace(min(jset(:,1)),max(jset(:,1)),length(jset(:,1))*25);
 
-    x_list = 0;
-    for i = 2:length(x)-1
+    [jset] = findUnique(jset)
+    [jset] = findUnique(jset)
 
-        if x(i-1)==x(i); 
-
-            x_list(end+1) = i;
-
-        end    
-
-    end
-    x_list = x_list(2:end);
-
-    y_list = 0;
-    for i = 2:length(y)-1
-
-        if y(i-1)==y(i);
-
-            y_list(end+1) = i;
-
-        end    
-
-    end
-    y_list = y_list(2:end);
-    e_list = sort([x_list,y_list]);
-    e_list = unique(e_list);
-
-    set(e_list,:) = [];
-    set(end-1,:) = [];
-
-    yy = spline(set(:,1),set(:,2),xx);
+    yy = pchip(jset(:,1),jset(:,2),xx);
 
     output = [xx',yy'];
+
 
 
