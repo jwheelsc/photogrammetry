4edfcd53bc651f1bcb776b873b662224262c2336
jwheelsc <jcrompto@sfu.ca>
2016-07-08 11:28:57 -0700

    for sets_Stats.m

--- sets_stats.m
+++ sets_stats.m
@@ -11,4 +11,15 @@ theta = '45'
 
 load([folder 'sl_pts_' num2str(theta) '_' setNum '.mat'])
 
+t_dist_bwp = []
 
+for i = 1:length(set_int)
+    
+    jset = set_int{i};
+    dif1 = jset(2:end,:)-jset(1:end-1,:);
+    dist_bwp = sqrt(sum(dif1.*dif1,2));
+    t_dist_bwp = [t_dist_bwp;dist_bwp];
+
+end
+
+hist(t_dist_bwp)
